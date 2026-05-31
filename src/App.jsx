2199c9c4905289cad39751096cfd7176ed352523
Louisian78 <louisianboltner@gmail.com>
2026-05-31 19:40:19 +0200

    Connect Nasa API with image display and history
    Connect DatePicker, NASA API, NasaImage and HistoryList through App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,79 @@
+import {useEffect, useState} from "react";
+
 import Header from "./components/Header/Header";
 import DatePicker from "./components/DatePicker/DatePicker";
 import NasaImage from "./components/NasaImage/NasaImage";
-import CategoryForm from "./components/CategoryForm"; 
+import CategoryForm from "./components/CategoryForm/CategoryForm"; 
 import CategoryList from "./components/CategoryList/CategoryList";
 import SavedImages from "./components/SavedImages/SavedImages";
 import HistoryList from "./components/HistoryList/HistoryList";
 
+import { fetchTodayNasaImage, fetchNasaImageByDate } from "./services/nasaApi";
+
 import "./App.css";
 
 function App() {
+  const[nasaData, setNasaData] = useState(null);
+  const[selectedDate, setSelectedDate] = useState("");
+  const[isLoading, setIsLoading] = useState (false);
+  const[error, setError] = useState(null);
+  const[history, setHistory] = useState([]);
+
+  useEffect(() => {
+    loadTodayImage();
+  }, []);
+
+  function addToHistory(data) {
+    const newHistoryItem = {
+      date: data.date,
+      title: data.title,
+    };
+
+    setHistory((oldHistory) => {
+      const filteredHistory = oldHistory.filter(
+        (item) => item.date !== newHistoryItem.date
+      );
+
+      return [newHistoryItem, ...filteredHistory];
+    });
+  }
+
+  async function loadTodayImage() {
+    try {
+      setIsLoading(true);
+      setError(null);
+
+      const data = await fetchTodayNasaImage();
+
+      setNasaData(data);
+      setSelectedDate(data.date);
+      addToHistory(data);
+    } catch(error) {
+      setError("Kunde tyvärr inte hämta dagens NASA-bild!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleDateChange(date) {
+    try {
+      setSelectedDate(date);
+      setIsLoading(true);
+      setError(null);
+
+      const data = await fetchNasaImageByDate(date);
+      setNasaData(data);
+      addToHistory(data);
+    } catch (error) {
+      setError("kunde tyvärr inte hämta NASA-bild för valt datum.")
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function handleHistoryClick(date) {
+    handleDateChange(date);
+  }
   return (
     <>
       <Header />
@@ -21,14 +86,24 @@ function App() {
 
         <div className="content-grid">
           <section className="main-content">
-            <DatePicker />
-            <NasaImage />
+            <DatePicker 
+              date={selectedDate}
+              onDateChange={handleDateChange}
+            />
+            <NasaImage 
+              data={nasaData}
+              isLoading={isLoading}
+              error={error}
+            />
           </section>
 
           <aside className="side-content">
           <CategoryForm />
           <CategoryList />
-          <HistoryList />
+          <HistoryList 
+            history={history}
+            onSelectDate={handleHistoryClick}
+          />
           </aside>  
         </div>
         <SavedImages /> 
